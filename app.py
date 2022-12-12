4119b0967f50a36a01edee20dbdab33000e6859c
Mikkel Teller <mikkelteller@gmail.com>
2022-12-12 21:07:20 +0100

    The webapp now allows users to sign up, login and logout.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, session, request
-from helpers import validate_login
+from helpers import get_db_connection, validate_login, validate_registration, add_user
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.secret_key = "htg924gt479ghau9w4q7tght4a9uqgthf"
@@ -8,20 +9,60 @@ app.secret_key = "htg924gt479ghau9w4q7tght4a9uqgthf"
 def index():
     if session.get("user_id") == None:
         return redirect("/login")
-    return render_template("views/index.html")
+    return render_template("views/index.html", session=session)
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("views/login.html")
+
     if request.method == "POST":
+
         username = request.form.get("username")
         password = request.form.get("password")
+
         if validate_login(username, password)[0]:
             session["user_id"] = validate_login(username, password)[1]
             return redirect("/")
+
         else:
-            return render_template("views/error.html", error_message="Username or password was incorrect"), 400
+            error_message = validate_login(username, password)[1]
+            return render_template("views/error.html", error_message=error_message), 400
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("views/register.html")
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+
+        if validate_registration(username, password, confirm_password)[0]:
+            # Add new user to the database
+            add_user(username, password)
+
+            # Automatically log the user in
+            conn = get_db_connection()
+            user = conn.execute("SELECT * FROM users WHERE username = ?", [username]).fetchall()
+            conn.close()
+
+            session["user_id"] = user[0]["user_id"]
+
+            return redirect("/")
+        
+        else:
+            error_message = validate_registration(username, password, confirm_password)[1]
+            print("sergo")
+            return render_template("views/error.html", error_message=error_message), 400
+
+@app.route("/logout")
+def logout():
+    session["user_id"] = None
+    return redirect("/login")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
